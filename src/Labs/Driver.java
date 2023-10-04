1061875574aec22cadecbecda6181f9f93299a92
swill93 <swill140@emich.edu>
2023-10-04 18:37:58 -0400

    Glad and Driver

--- src/Labs/Driver.java
+++ src/Labs/Driver.java
@@ -7,5 +7,6 @@ public static void main (System []args) {
  System.out.println("Our Team is getting started.");
  Glad.glad();
  
+ 
   }
 }
---
+++ src/Labs/Glad.java
@@ -0,0 +1,8 @@
+
+public class Glad {
+	public static void glad() {
+		System.out.println("We are feeling glad");
+		
+	}
+
+}
